7d9b58262347d90062924f1872fac017d3a3ce52
tsaude <tsaude@live.com>
2014-03-18 13:54:57 -0500

    added test folder

---
+++ AddressBook/test/edu/gustavus/MCS270/addressbook/Test.java
@@ -0,0 +1,12 @@
+package edu.gustavus.MCS270.addressbook;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
